99bff9df6ad1849ab513c48463b24cc5cd042613
ATQQ <2604395430@qq.com>
2019-07-14 10:57:41 +0800

    :rocket:项目初始创建文件

---
+++ pages/index/index.vue
@@ -0,0 +1,45 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view>
+			<text class="title">{{title}}</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello123'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="less" scoped>
+	.content {
+		text-align: center;
+		height: 400upx;
+		p{
+			color:red;
+		}
+	}
+
+	.logo {
+		height: 200upx;
+		width: 200upx;
+		margin-top: 200upx;
+	}
+
+	.title {
+		font-size: 36upx;
+		color: #8f8f94;
+	}
+</style>
